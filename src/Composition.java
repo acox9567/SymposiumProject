58b7de521d05fc019e3f78614db95b99a16ce7e7
Xander <acox9567@bths.edu>
2019-02-15 10:13:46 -0500

    zxcv

--- src/Composition.java
+++ src/Composition.java
@@ -1,11 +1,52 @@
+//Alexander Cox
+//Friday, February 15, 2019
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+
 public class Composition {
 
-    private String title;
     private String key;
+    private int tempo;
+    private String title;
+    private List<Pattern> patterns = new ArrayList<Pattern>();
 
-    public Composition(String title, String key)
+    public Composition(String key, int tempo, String title)
     {
-        this.title = title;
         this.key = key;
+        this.tempo = tempo;
+        this.title = title;
+    }
+
+    public void addPattern(Pattern pattern)
+    {
+        patterns.add(pattern);
+    }
+
+    public void playBack()
+    {
+        Player player = new Player();
+
+        for (int i = 0; i < patterns.size(); i++)
+        {
+            player.play(patterns.get(i));
+        }
+    }
+
+    public String getKey()
+    {
+        return this.key;
+    }
+
+    public int getTempo()
+    {
+        return this.tempo;
+    }
+
+    public String getTitle()
+    {
+        return this.title;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
+import org.jfugue.rhythm.Rhythm;
 
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Synthesizer;
@@ -18,15 +19,21 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
-        Composition AutumnLeaves = new Composition("Autumn Leaves", "Gminor");
+        Composition autumnLeaves = new Composition("Gmin", 180, "Autumn Leaves");
 
-        Pattern pattern = new Pattern("T[Presto]");
-        pattern.add("V0 I[Piano]         Rw              | C5min7w | F4dom7w          | Bb4maj7w | Eb5maj7w         | A5min7w | D5dom7w          | G4min7w | G4min7w");
-        pattern.add("V1 I[Trumpet]       Rq G5q A5q Bb5q | Eb6w    - Eb6q F5q G5q A5q | D6h D6h  - D6q Eb5q F5q G5q | C6w     - C6q D5q E5q F#5q | Bb5w    | Bb5w");
+        Pattern A = new Pattern();
+        A.setTempo(autumnLeaves.getTempo());
+        A.add("V0 I[Piano]   Rw              | C5min7w | F4dom7w          | Bb4maj7w | Eb5maj7w         | A4min7w | D5dom7w          | G4min7w | G4min7w         | C5min7w | F5dom7w          | Bb4maj7w | Eb5maj7w         | A4min7w | D5dom7w          | G4min7w  | G4min7w");
+        A.add("V1 I[Trumpet] Rq G5q A5q Bb5q | Eb6w    - Eb6q F5q G5q A5q | D6h D6h  - D6q Eb5q F5q G5q | C6w     - C6q D5q E5q F#5q | Bb5w    | Rq G5q A5q Bb5q | Eb6w    - Eb6q F5q G5q A5q | D6h D6h  - D6q Eb5q F5q G5q | C6w     - C6q A5q C6q Bb5q | G5w      | Rh F#5 G6");
 
-        Player player = new Player();
-        player.play(pattern);
+        autumnLeaves.addPattern(A);
 
+        Pattern B = new Pattern();
+        B.setTempo(autumnLeaves.getTempo());
+        A.add("V0 I[Piano]   A4min7w     | D5dom7w | G4min7w | G4min7w | C5min7w | F5dom7w | Bb4maj7w | Eb5maj7w | A4min7w | D5dom7w | G4min7h C5min7h | F5dom7h Bb4dom7 | Eb5maj7w | A4min7b5h D5dom7h | G4min7w - G4min7w");
+        A.add("V1 I[Trumpet] A5q D5q A5h - A5q");
+
+        autumnLeaves.playBack();
     }
 
 
